5cbdafc61fcf360776b78814c43cf529c089cc05
Michele Lacchia <michelelacchia@gmail.com>
2015-10-20 23:29:09 +0200

    docs(main): add titles and description

--- src/Argon.hs
+++ src/Argon.hs
@@ -1,6 +1,26 @@
-module Argon (AnalysisResult, ComplexityBlock, OutputMode(..)
-             , ResultsOptions(..), parseCode, order, filterResults, export)
-    where
+-- |
+-- Module:      Argon
+-- Copyright:   (c) 2015 Michele Lacchia
+-- License:     ISC
+-- Maintainer:  Michele Lacchia <michelelacchia@gmail.com>
+-- Stability:   alpha
+-- Portability: portable
+--
+-- Programmatic interface to Argon.
+module Argon
+    (
+    -- * Types
+      AnalysisResult
+    , ComplexityBlock
+    , OutputMode(..)
+    , ResultsOptions(..)
+    -- * Parsing
+    , parseCode
+    -- * Manipulating results
+    , order
+    , filterResults
+    , export
+    ) where
 
 import Argon.Parser (parseCode)
 import Argon.Results (order, export, filterResults)
