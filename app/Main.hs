20392841c4fc2666caa2dfdc274e720d4bc5f72d
Michele Lacchia <michelelacchia@gmail.com>
2015-10-21 22:12:00 +0200

    feat(app): descend into paths to look for .hs files; fixes #4

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,20 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE QuasiQuotes #-}
 module Main where
 
+import Data.List (foldl1', isSuffixOf)
+import Data.Sequence (Seq)
+import qualified Data.Sequence as S
+import Data.Foldable (toList)
 import System.Environment (getArgs)
+import System.FilePath ((</>))
+import System.Directory
+import System.Directory.PathWalk
 import System.Console.Docopt
 import Argon
+#if __GLASGOW_HASKELL__ < 710
+import Control.Applicative ((<$>))
+#endif
 
 
 patterns :: Docopt
@@ -15,10 +26,27 @@ getArgOrExit = getArgOrExitWith patterns
 getOpt :: Arguments -> String -> String -> String
 getOpt args def opt = getArgWithDefault args def $ longOption opt
 
-processFile :: String -> IO (FilePath, AnalysisResult)
+haskellFiles :: Seq FilePath -> Seq FilePath
+haskellFiles = S.filter (".hs" `isSuffixOf`)
+
+processFile :: FilePath -> IO (FilePath, AnalysisResult)
 processFile path = do
     contents <- readFile path
     parseCode (Just path) contents
+
+findSourceFiles :: FilePath -> IO (Seq FilePath)
+findSourceFiles path =
+    pathWalkAccumulate path $ \dir _ files ->
+        return $ fmap (dir </>) $ haskellFiles $ S.fromList files
+
+descend :: FilePath -> IO (Seq FilePath)
+descend path = do
+    isFile <- doesFileExist path
+    if isFile then return $ haskellFiles $ S.singleton path
+              else findSourceFiles path
+
+allFiles :: [FilePath] -> IO (Seq FilePath)
+allFiles paths = foldl1' mappend <$> mapM descend paths
 
 readConfig :: Arguments -> IO Config
 readConfig args = return
@@ -31,10 +59,10 @@ readConfig args = return
                               else Colored
     }
 
-
 main :: IO ()
 main = do
     args <- parseArgsOrExit patterns =<< getArgs
-    res  <- mapM processFile $ args `getAllArgs` argument "paths"
+    ins  <- allFiles $ args `getAllArgs` argument "paths"
+    res  <- mapM processFile $ toList ins
     conf <- readConfig args
     putStr $ export conf $ map (filterResults conf) res
