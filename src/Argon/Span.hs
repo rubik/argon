9647380f34d89038ec02afa86dab30de0cd91dea
Michele Lacchia <michelelacchia@gmail.com>
2015-10-25 23:00:02 +0100

    fix(Span): make srcSpanToSpan safe

--- src/Argon/Span.hs
+++ src/Argon/Span.hs
@@ -3,16 +3,20 @@ module Argon.Span (Span, srcSpanToSpan, spanToString, tagMsg)
 
 import Text.Printf (printf)
 import Control.Arrow ((&&&))
-import qualified SrcLoc as GHC
+
+import qualified SrcLoc     as GHC
+import qualified FastString as GHC
 
 type Span = (Int, Int, Int, Int)
 
 
 srcSpanToSpan :: GHC.SrcSpan -> Span
 srcSpanToSpan ss = (sl, sc, el, ec)
-    where lloc = (GHC.srcLocLine &&& GHC.srcLocCol) . (\(GHC.RealSrcLoc z) -> z)
-          (sl, sc) = lloc $ GHC.srcSpanStart ss -- TODO: Make it safe
+    where (sl, sc) = lloc $ GHC.srcSpanStart ss
           (el, ec) = lloc $ GHC.srcSpanEnd ss
+          lloc = (GHC.srcLocLine &&& GHC.srcLocCol) . toRealSrcLoc
+          toRealSrcLoc (GHC.RealSrcLoc z) = z
+          toRealSrcLoc _ = GHC.mkRealSrcLoc (GHC.mkFastString "no info") 1 1
 
 spanToString :: Span -> String
 spanToString (l, c, _, _) = printf "%d:%d" l c
