62b59b66a44c5df523e13c9543a67cbf4d643a05
Michele Lacchia <michelelacchia@gmail.com>
2015-10-26 10:19:36 +0100

    fix(test): fix spec and add preliminar test data

--- test/ArgonSpec.hs
+++ test/ArgonSpec.hs
@@ -8,18 +8,25 @@ import Control.Applicative ((<$>), (<*>))
 #endif
 import Test.Hspec
 import Test.QuickCheck
+import System.FilePath ((</>))
 import Argon
 
 instance Arbitrary ComplexityBlock where
-    arbitrary = (\a b c d -> CC (a, b, c, d)) <$> arbitrary
-                                              <*> arbitrary
-                                              <*> arbitrary
-                                              <*> arbitrary
+    arbitrary = (\a b c -> CC (a, b, c)) <$> arbitrary
+                                         <*> arbitrary
+                                         <*> arbitrary
     shrink (CC t) = map CC $ shrink t
 
 
-shouldParse :: String -> AnalysisResult -> Expectation
-shouldParse s r = parseCode (Just "fname") s `shouldReturn` ("fname", r)
+ones :: Span
+ones = (1, 1, 1, 1)
+
+sp :: Int -> Int -> Span
+sp s e = (s, 1, e, 1)
+
+shouldAnalyze :: String -> AnalysisResult -> Expectation
+shouldAnalyze f r = analyze path `shouldReturn` (path, r)
+    where path = "test" </> "data" </> f
 
 spec :: Spec
 spec = do
@@ -27,22 +34,18 @@ spec = do
         it "does not error on empty list" $
             order [] `shouldBe` []
         it "orders by complexity (descending)" $
-            order [CC (1, 1, "f", 1), CC (2, 1, "f", 2)] `shouldBe`
-                [CC (2, 1, "f", 2), CC (1, 1, "f", 1)]
+            order [CC (ones, "f", 1), CC (sp 2 1, "f", 2)] `shouldBe`
+                [CC (sp 2 1, "f", 2), CC (ones, "f", 1)]
         it "orders by lines (ascending)" $
-            order [CC (11, 1, "f", 3), CC (1, 1, "f", 3)] `shouldBe`
-                [CC (1, 1, "f", 3), CC (11, 1, "f", 3)]
+            order [CC (sp 11 1, "f", 3), CC (ones, "f", 3)] `shouldBe`
+                [CC (ones, "f", 3), CC (sp 11 1, "f", 3)]
         it "orders by function name (ascending)" $
-            order [CC (11, 1, "g", 3), CC (11, 1, "f", 3)] `shouldBe`
-                [CC (11, 1, "f", 3), CC (11, 1, "g", 3)]
-        it "does not remove or add elements" $
-            property $ \xs -> sort xs == sort (order xs)
+            order [CC (sp 11 1, "g", 3), CC (sp 11 1, "f", 3)] `shouldBe`
+                [CC (sp 11 1, "f", 3), CC (sp 11 1, "g", 3)]
+        it "is idempotent" $
+            property $ \xs -> order xs == order (order xs)
     describe "parseCode" $ do
         it "accounts for case" $
-            unlines [ "f n = case n of"
-                    , "        2 -> 24"
-                    , "        3 -> 27"
-                    , "        _ -> 49"] `shouldParse` Right [CC (1, 1, "f", 3)]
+            "case.hs" `shouldAnalyze` Right [CC ((1, 1, 1, 5), "func", 3)]
         it "accounts for if..then..else" $
-            "f n = if n == 4 then 24 else 20" `shouldParse`
-                Right [CC (1, 1, "f", 2)]
+            "ifthenelse.hs" `shouldAnalyze` Right [CC ((1, 1, 1, 2), "f", 2)]
