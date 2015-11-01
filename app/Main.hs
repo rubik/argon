62cef6bcdef4557669d5e3bf02ac5eb8691cd1fb
Michele Lacchia <michelelacchia@gmail.com>
2015-11-01 20:39:52 +0100

    fix(Main): fix JSON output

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RankNTypes #-}
 module Main where
 
 import Data.List (foldl1', isSuffixOf)
@@ -9,7 +10,10 @@ import qualified Data.Sequence as S
 import Data.Monoid (mappend)
 import Control.Applicative ((<$>))
 #endif
+import Data.Aeson (encode)
+import qualified Data.ByteString.Lazy as B
 import Pipes
+import Pipes.Parse (Parser, draw, evalStateT)
 import qualified Pipes.Prelude as P
 import System.Environment (getArgs)
 import System.FilePath ((</>))
@@ -63,14 +67,41 @@ filterNulls (_, r) = case r of
                        Right [] -> False
                        _        -> True
 
+jsonParser :: Parser (FilePath, AnalysisResult) IO ()
+jsonParser = do
+    lift $ putChar '['
+    first <- draw
+    case first of
+      Nothing -> lift $ putChar ']'
+      Just el -> do
+          lift $ B.putStr $ encode el
+          let loop = do
+                mx <- draw
+                case mx of
+                  Nothing -> lift $ putChar ']'
+                  Just x  -> lift (putChar ',' *> B.putStr (encode x)) *> loop
+          loop
+
+textParser :: Config -> Parser (FilePath, AnalysisResult) IO ()
+textParser conf = do
+    first <- draw
+    case first of
+      Nothing -> return ()
+      Just el -> lift (putStrLn $ export conf el) *> textParser conf
+
+exportStream :: Config -> Parser (FilePath, AnalysisResult) IO ()
+exportStream conf =
+    case outputMode conf of
+      JSON -> jsonParser
+      _    -> textParser conf
+
 main :: IO ()
 main = do
     args <- parseArgsOrExit patterns =<< getArgs
     ins  <- allFiles $ args `getAllArgs` argument "paths"
     let conf = readConfig args
-    runEffect $ each ins
-             >-> P.mapM analyze
-             >-> P.map (filterResults conf)
-             >-> P.filter filterNulls
-             >-> P.map (export conf)
-             >-> P.stdoutLn
+        pipeline = each ins
+                >-> P.mapM analyze
+                >-> P.map (filterResults conf)
+                >-> P.filter filterNulls
+    evalStateT (exportStream conf) pipeline
