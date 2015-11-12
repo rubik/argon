410f891378e19c6e2e19f989e3ef020816908fc0
Michele Lacchia <michelelacchia@gmail.com>
2015-11-12 22:44:34 +0100

    fix(travis-ci): don't reset stack if it's already there

--- travis-install-stack.sh
+++ travis-install-stack.sh
@@ -6,9 +6,9 @@ STACK_HOME=$HOME/stack
 echo 'Installing Stack'
 if [[ ! -d $STACK_HOME ]]; then
     git clone --depth 300 https://github.com/commercialhaskell/stack $STACK_HOME
+    git reset --hard 24a461c950c5c8719ff427ab5a3b1494530b62e5
 fi
 
 cd $STACK_HOME
-git reset --hard 24a461c950c5c8719ff427ab5a3b1494530b62e5
 echo 'Compiling Stack'
 stack install
